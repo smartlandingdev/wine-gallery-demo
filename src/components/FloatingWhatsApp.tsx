85573a5b1369a97a9749db74e6d6ae72ce02404c
Majuks <mariajnascimento04@gmail.com>
2025-10-02 21:44:05 -0300

    sem zap pulsando

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -34,9 +34,6 @@ export const FloatingWhatsApp = () => {
           </div>
         )}
       </button>
-
-      {/* Pulse animation */}
-      <div className="absolute inset-0 bg-green-500 rounded-full animate-ping opacity-75"></div>
     </div>
   );
 };
